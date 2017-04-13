0418192bcc0d4304cad10e8f0e0f33115a330e5d
Matias Vera <matias.veraaravena@gmail.com>
2017-04-13 01:02:37 -0300

    current User id

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -14,11 +14,11 @@
 */
 export let version: string = '0.4.9.8';
 
-export let apiUrl: string = 'http://192.168.1.175:3000';//dev
+// export let apiUrl: string = 'http://192.168.1.175:3000';//dev
 
 // export let apiUrl: string = 'https://demoapi.innovacolegio.cl';//remote dev
 
-// export let apiUrl: string = 'https://api.innovacolegio.cl';//production
+export let apiUrl: string = 'https://api.innovacolegio.cl';//production
 
 export let rolesDocentes = [
   {'rol':'profesor'},
