257f2811121b3b0ce22fda0aeae1b50f58803ef1
Matias Vera <matias.veraaravena@gmail.com>
2017-05-30 12:29:07 -0400

    electivo/notas: added electivo info; fixed bugs with funcionarios

--- src/app/components/libros/electivos/notas-electivo/notas-electivo.component.ts
+++ src/app/components/libros/electivos/notas-electivo/notas-electivo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+
+import { CursosService } from '../../../../services/libros/cursos.service';
+import {ConfiguracionService} from '../../../../services/sistema/configuracion.service';
+import {PlanDeEstudiosService} from "../../../../services/sistema/configuraciones/plan-de-estudios.service";
 
 @Component({
   selector: 'app-notas-electivo',
@@ -8,6 +12,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class NotasElectivoComponent implements OnInit {
 
+  electivo: any;
+  private planesDeEstudio = [];
+
+  id: number;
+  private sub: any;
+
   private currentTabPath: string = '';
   tabs = [
     {"id":1,"path":'ver',"label":"Ver","icon":"icon-eye"},
@@ -15,8 +25,11 @@ export class NotasElectivoComponent implements OnInit {
   ];
 
   constructor(
-    private route: ActivatedRoute,
     private router: Router,
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private configuracionService: ConfiguracionService,
+    private planesDeEstudiosService: PlanDeEstudiosService,
   ) {
     this.router.events.subscribe((res) => {
         this.currentTabPath = this.route.children[0].toString();
@@ -25,6 +38,45 @@ export class NotasElectivoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.sub = this.route.parent.params.subscribe(params => {
+      this.id = params['id'];
+    });
+
+    this.route.parent.params
+      .switchMap((params: Params) => this.cursosService.getElectivoById(+params['id']))
+      .subscribe((res) => {
+        this.electivo = res.electivo;
+        this.configuracionService.getConfiguraciones().subscribe(configs => {
+          let configId = configs.find(c => c.glosa == 'Planes de Estudio y Tipos de Enseñanza' && c.colegio_id == +JSON.parse(localStorage.getItem('currentUser')).colegioId).id;
+
+          this.planesDeEstudiosService.getConfigPlanesDeEstudio(configId).subscribe(res => {
+            if(res && res.planes && res.planes.length>0){
+
+              this.planesDeEstudio = res.planes;
+            } else {
+              let currentRol = +atob(atob(JSON.parse(localStorage.getItem('currentUser')).rol))[5];
+              if(currentRol==4||currentRol==5){
+                this.router.navigate(['app/alerta-configuracion',3]);
+              } else {
+                this.router.navigate(['app/sistema/configuracion/planes-ensenanza']);
+              }
+            }
+          })
+        })
+      });
+  }
+
+  getPlanEstudiosByPlanId(planId: number): string{
+    let plan = this.planesDeEstudio.find(p => p.id == planId);
+    if(plan){
+      return plan.decreto.length>60 ? plan.decreto.substring(0,plan.decreto.length-28)+'...' : plan.decreto;
+    }
+    return '';
+  }
+
+  goBack(): void {
+    //this.location.back navigates inside tabs, bad for tab logic.
+    this.router.navigate(['./'],{relativeTo: this.route.parent.parent});
   }
 
 }
